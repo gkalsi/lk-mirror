3a1d25e07ac908475787ded73a03c1c86b1669a9
Gurjant Kalsi <me@gurjantkalsi.com>
2016-07-26 16:17:52 -0700

    Temp commit of initial import from Xinu USB stack/RPi Driver.

---
+++ external/platform/bcm28xx/usbcore/include/platform/usbcore/usb_subsystem.h
@@ -0,0 +1,16 @@
+/**
+ * @file usb_subsystem.h
+ * @ingroup usb
+ *
+ * Interface to the USB (Universal Serial Bus) subsystem from other operating
+ * system code.
+ */
+/* Embedded Xinu, Copyright (C) 2013.  All rights reserved. */
+
+#ifndef _USB_SUBSSYSTEM_H_
+#define _USB_SUBSSYSTEM_H_
+
+void usbinit(void);
+void usbinfo(void);
+
+#endif /* _USB_SUBSSYSTEM_H_ */
---
+++ external/platform/bcm28xx/usbcore/rules.mk
@@ -0,0 +1,13 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+GLOBAL_INCLUDES += $(LOCAL_DIR)/include
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/usbcore.c \
+	$(LOCAL_DIR)/usbdebug.c \
+	$(LOCAL_DIR)/usb_dwc_hcd.c \
+	$(LOCAL_DIR)/usbhub.c
+
+include make/module.mk
